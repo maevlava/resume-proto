d7b24ef6ae9f95bcf2cd3ee4d2e10130d069a1f7
maevlava <maevlava@gmail.com>
2025-10-11 11:36:29 +0700

    Adds authentication functionality
    Implements user authentication using a backend API.
    
    Adds login, logout, and session validation functionality.
    Simulates loading state during authentication.

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,11 +1,89 @@
+import {useEffect, useState} from "react";
+
 export const meta = () => ([
-    { title: "Resume Proto | Auth"},
-    { name: "description", content: "Log into your account"}
+    {title: "Resume Proto | Auth"},
+    {name: "description", content: "Log into your account"}
 ])
 
 
 export const Auth = () => {
-    // check auth loading, auth
+    const [isLoading, setIsLoading] = useState(true);
+    const [isAuth, setIsAuth] = useState(false);
+
+    async function handleAuth() {
+        // simulate 3 seconds remove on productionn
+        await new Promise(resolve => setTimeout(resolve, 3000))
+
+        let response = await fetch('http://localhost:8080/api/v1/auth/validate', {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return response.status === 200
+    }
+
+    async function SignIn(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const email = form.get('email')
+        const password = form.get('password')
+
+        let response = await fetch('http://localhost:8080/api/v1/auth/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        console.log(email, password)
+        if(!response.ok) {
+            const msg = await response.text()
+            throw new Error(msg || "Invalid credentials")
+        }
+        console.log(response)
+        setIsAuth(true)
+    }
+
+    const SignOut = () => {
+        let response = fetch('http://localhost:8080/api/v1/auth/logout', {
+           method: 'GET',
+           credentials: 'include',
+        })
+        console.log(response)
+    }
+
+    useEffect(() => {
+        let authorize = async () => {
+            const isAuth = await handleAuth()
+            setIsLoading(false)
+            setIsAuth(isAuth)
+        }
+        authorize()
+    }, [])
+    let content
+    if (isLoading) {
+        content = <button className={"auth-button animate-pulse"}>Signing you in...</button>
+    } else if (isAuth) {
+        content =
+            <button type={"submit"} className={"auth-button"} onClick={SignOut}><p>Sign Out</p></button>
+    } else {
+        content =
+            <form className={"gradient-border w-full mx-auto bg-white"} onSubmit={SignIn}>
+                <div className={"form-div"}>
+                    <label htmlFor={"email"}>Email</label>
+                    <input id={"email"} name={"email"} type={"email"} placeholder={"test@example.com"} required/>
+                </div>
+                <div className={"form-div"}>
+                    <label htmlFor={"password"}>Password</label>
+                    <input id={"password"} name={"password"} type={"password"} placeholder={"********"} required/>
+                </div>
+                <p className={"text-dark-200 text-sm mt-4 text-center"}>Don't have an account?</p>
+                <button className={"auth-button"}><p>Sign In</p></button>
+            </form>
+    }
 
     return (
         <div className={"bg-[url('/images/bg-auth.svg')] min-h-screen bg-cover flex items-center justify-center"}>
@@ -15,9 +93,7 @@ export const Auth = () => {
                         <h1>Welcome</h1>
                         <h2>Log In to Continue Your Job Journey</h2>
                     </div>
-                {/*    if loading render loading button*/}
-                {/*    if not loading, and not auth render sign in button*/}
-                {/*    if not loading, and auth render sign out button*/}
+                    {content}
                 </section>
             </div>
         </div>
@@ -25,3 +101,4 @@ export const Auth = () => {
 };
 
 export default Auth;
+
