587d378ff539d6335f2c0a58bd7e896dea624893
maevlava <maevlava@gmail.com>
2025-11-01 19:31:10 +0700

    Improves home page UI and UX
    Updates the home page to display a message when no resumes are found.
    Adds a link to upload a resume when no resumes are available.
    Conditionally renders company and job title in ResumeCard.
    Removes unused resume images.
    Adds a loading indicator while fetching resumes.

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -2,13 +2,14 @@ import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 import {STATIC_BASE_URL} from "../../constants";
 
-export const ResumeCard = ({resume : {id, jobTitle, companyName, feedback, imagePath}}: {resume: Resume}) => {
+export const ResumeCard = ({resume: {id, jobTitle, companyName, feedback, imagePath}}: { resume: Resume }) => {
     return (
         <Link to={`/resume/${id}`} className={"resume-card animate-in fade-in duration-1000"}>
             <div className={"resume-card-header py-16"}>
                 <div className={"flex flex-col gap-2"}>
-                    <h2 className={"!text-black font-bold break-words"}>{companyName}</h2>
-                    <h3 className={"!text-gray-500 text-lg break-words"}>{jobTitle}</h3>
+                    {companyName && <h2 className={"!text-black font-bold break-words"}>{companyName}</h2>}
+                    {jobTitle && <h3 className={"!text-gray-500 text-lg break-words"}>{jobTitle}</h3>}
+                    {!companyName && !jobTitle && <h3 className={"!text-black font-bold"}>Resume</h3>}
                 </div>
                 <div className={"flex-shrink-0"}>
                     <ScoreCircle score={feedback!!.overallScore}/>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,52 +1,69 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
 import Navbar from "~/components/Navbar";
-import { ResumeCard } from "~/components/ResumeCard";
+import {ResumeCard} from "~/components/ResumeCard";
 import {useAuth} from "../../hooks/useAuth";
-import {useLocation, useNavigate} from "react-router";
+import {Link, useLocation, useNavigate} from "react-router";
 import {useEffect, useState} from "react";
 import {useStore} from "../../hooks/useStore";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Resume-proto" },
-    { name: "description", content: "Smart feedback for resumes" },
-  ];
+    return [
+        {title: "Resume-proto"},
+        {name: "description", content: "Smart feedback for resumes"},
+    ];
 }
 
 export default function Home() {
-  const {store} = useStore()
-  const [resumes, setResumes] = useState<Resume[]>([])
-  const {isLoading, auth} = useAuth()
-  const navigate = useNavigate()
+    const {store} = useStore()
+    const [resumes, setResumes] = useState<Resume[]>([])
+    const {isLoading, auth} = useAuth()
+    const navigate = useNavigate()
 
-  useEffect(() => {
-    if(!auth.isAuthenticated && !isLoading) navigate("/auth?next=/")
-  }, [auth.isAuthenticated, isLoading])
+    useEffect(() => {
+        if (!auth.isAuthenticated && !isLoading) navigate("/auth?next=/")
+    }, [auth.isAuthenticated, isLoading])
 
-  useEffect(() => {
-    (async () => {
-      const resumes = await store.getResumes()
-      setResumes(resumes)
-    })()
-  }, [])
+    useEffect(() => {
+        (async () => {
+            const resumes = await store.getResumes()
+            setResumes(resumes)
+        })()
+    }, [])
 
-  return <main className={"min-h-screen bg-[url('/images/bg-main.svg')] bg-cover"}>
-    <Navbar/>
+    return <main className={"min-h-screen bg-[url('/images/bg-main.svg')] bg-cover"}>
+        <Navbar/>
 
-    <section className={"main-section"}>
-      <div className={"page-heading"}>
-          <h1>Track Your Applications & Resume Ratings</h1>
-          <h2>Review your submissions and check AI-powered feedback.</h2>
-      </div>
+        <section className={"main-section"}>
+            <div className={"page-heading py-16"}>
+                <h1>Track Your Applications & Resume Ratings</h1>
+                {!isLoading && resumes.length === 0 ? (
+                    <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                ) : (
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                )}
+            </div>
+            {isLoading && (
+                <div className={"flex flex-col items-center justify-center"}>
+                    <img src={"/images/resume-scan-2.gif"} alt={"loading"} className={"w-[200px]"}/>
+                </div>
+            )}
 
-    {resumes.length > 0 && (
-     <div className={"resumes-section"}>
-       {resumes.map((resume) => (
-           <ResumeCard key={resume.id} resume={resume}/>
-       ))}
-     </div>
-    )}
-    </section>
+            {resumes.length > 0 && (
+                <div className={"resumes-section"}>
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    ))}
+                </div>
+            )}
 
-  </main>
+            {!isLoading && resumes.length === 0 && (
+                <div className={"flex flex-col items-center justify-center mt-10 gap-4"}>
+                    <Link to={"/upload"} className={"primary-button w-fit text-xl font-semibold"}>
+                        Upload Resume
+                    </Link>
+                </div>
+            )}
+        </section>
+
+    </main>
 }
